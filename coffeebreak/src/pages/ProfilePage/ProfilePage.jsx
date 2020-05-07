383124a084fba0410aa2f2b537f3dc9b582fb3a0
Yasaman Loghmani <yasamanloghmani@Yasamans-MacBook-Pro.local>
2020-05-07 18:03:08 -0400

    Add profile page delete acc

--- coffeebreak/src/pages/ProfilePage/ProfilePage.jsx
+++ coffeebreak/src/pages/ProfilePage/ProfilePage.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import userService from "../../utils/userService";
 import { Redirect } from "react-router-dom";
+import './ProfilePage.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin , faGithub } from "@fortawesome/free-brands-svg-icons"
 
 
 export class ProfilePage extends Component {
@@ -8,28 +11,25 @@ export class ProfilePage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: `${this.props.user.name}`,
-            limitOfCoffee: `${this.props.user.limitOfCoffee}`,
-            limitOfExpense : `${this.props.user.limitOfExpense}`,
             isDeleted: false
         };
     }
 
-    handleChange = evt => {
-        this.setState({
-            [evt.target.name]: evt.target.value
-        });
-    };
+    // handleChange = evt => {
+    //     this.setState({
+    //         [evt.target.name]: evt.target.value
+    //     });
+    // };
 
-    handleSubmit = async evt => {
-        evt.preventDefault();
-        try {
-            await userService.updateUser(this.state);
-            this.props.history.push("/profile");
-        } catch (err) {
-            console.log(err);
-        }
-    };
+    // handleSubmit = async evt => {
+    //     evt.preventDefault();
+    //     try {
+    //         await userService.updateUser(this.state);
+    //         this.props.history.push("/profile");
+    //     } catch (err) {
+    //         console.log(err);
+    //     }
+    // };
 
     handleDelete = async evt => {
         evt.preventDefault();
@@ -48,87 +48,31 @@ export class ProfilePage extends Component {
             return <Redirect to="/" />;
         } else {
             return (
-                // <div className="ProfilePage">
-                //     <div className="">
-                //         <header>
-                //             <h2 className="basic-title font-weight-bold profile-title">
-                //                 {this.props.user.name}'s Profile
-                //             </h2>
-                //         </header>
-                //         <br />
-                //         <form
-                //             onSubmit={this.handleSubmit}
-                //             className="form-horizontal"
-                //         >
-                //             <div className="form-group row mx-auto">
-                //                 <label
-                //                     htmlFor="name"
-                //                     className="col-sm-3 col-form-label"
-                //                 >
-                //                     Name:
-                //                 </label>
-                //                 <input
-                //                     type="text"
-                //                     className="form-control col-sm-6 mb-4"
-                //                     id="name"
-                //                     value={this.state.name}
-                //                     name="name"
-                //                     onChange={this.handleChange}
-                //                 />
-                //             </div>
-                //             <div className="form-group row mx-auto">
-                //                 <label
-                //                     htmlFor="limitOfCoffee"
-                //                     className="col-sm-3 col-form-label"
-                //                 >
-                //                     Limit Of Coffee Per Day:
-                //                 </label>
-                //                 <input
-                //                     type="text"
-                //                     className="form-control col-sm-6 mb-4"
-                //                     id="limitOfCoffee"
-                //                     value={this.state.limitOfCoffee}
-                //                     name="limitOfCoffee"
-                //                     onChange={this.handleChange}
-                //                 />
-                //             </div>
-                //             <div className="form-group row mx-auto">
-                //                 <label
-                //                     htmlFor="limitOfExpense"
-                //                     className="col-sm-3 col-form-label"
-                //                 >
-                //                     Limit Of Expense Per Day:
-                //                 </label>
-                //                 <input
-                //                     type="text"
-                //                     className="form-control col-sm-6 mb-4"
-                //                     name="limitOfExpense"
-                //                     value={this.state.limitOfExpense}
-                //                     onChange={this.handleChange}
-                //                 />
-                //             </div>
-                //             <div className="d-flex justify-content-center">
-                //                 <button
-                //                     type="submit"
-                //                     className="btn"
-                //                     onClick={this.handleSubmit}
-                //                 >
-                //                     Update
-                //                 </button>
-                //                 <button
-                //                     className="btn"
-                //                     onClick={this.handleDelete}
-                //                 >
-                //                     Delete Account
-                //                 </button>
-                //             </div>
-                            
-                //         </form>
-                //     </div>
+                <div className="ProfilePage">
+                    <div className="">
+                         <header>
+                             <h2 className="basic-title font-weight-bold profile-title">
+                                 {this.props.user.name}'s Profile
+                             </h2>
+                         </header>
+                                <h4 className="headermargin">Do you want to delete your account ?</h4>
+                                <button
+                                    className="btn btn-default"
+                                    onClick={this.handleDelete}
+                                >
+                                    Yes , Delete Account!
+                                </button>
+                        
+                    </div>
+                    <div className='about'>
+                    <h5>Developed by <span>Yasaman Loghmani</span></h5>
+                    <a href='https://linkedin.com/in/yasamanloghmani' className='Menu-link'><FontAwesomeIcon className='icons' icon={faLinkedin} /></a>
+                    <a href='https://https://github.com/yasamanloghmani' className='Menu-link'><FontAwesomeIcon className='icons' icon={faGithub} /></a>
+
+                    </div>
                     
-                //     <br />
-                // </div>
-                <p>Hi</p>
+                </div>
+                
             );
         }
     }
