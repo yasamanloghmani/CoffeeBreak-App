18a7d5854f0c3ac4ab8855ffb44af761ed44d8f0
Yasaman Loghmani <yasamanloghmani@Yasamans-MacBook-Pro.local>
2020-05-07 20:02:47 -0400

    change folder

---
+++ build/precache-manifest.ac16b8492c11194952d2e601a1cc0be2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab479dfefa9b1e2def06dbacba887ba3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c37d24fd4a38706c02a5",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "0c28183cbf8fecff9c38",
+    "url": "/static/css/main.a6980a73.chunk.css"
+  },
+  {
+    "revision": "c37d24fd4a38706c02a5",
+    "url": "/static/js/2.fa483368.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fa483368.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c28183cbf8fecff9c38",
+    "url": "/static/js/main.1d8feb32.chunk.js"
+  },
+  {
+    "revision": "d551ad26bc4b3f401e04",
+    "url": "/static/js/runtime-main.a93a7b9e.js"
+  }
+]);
